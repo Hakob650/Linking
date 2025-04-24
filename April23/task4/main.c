d370c0e85496890d57d8ecf91d556f359dbc4a5b
Hakob650 <hakob11meneshian@gmail.com>
2025-04-24 20:05:36 +0400

    Update main.c

--- April23/task4/main.c
+++ April23/task4/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "math.h"
+
+int main(){
+        int sum=add(3,8);
+        int difference=subtract(8,3);
+
+        printf("Sum=%d\n",sum);
+        printf("Difference=%d\n",difference);
+
+        return 0;
+}
 
